0baa5af04dbe6f1e94b7b8fdc635ca30bab0d06e
Alex Acebo <aacebowork@gmail.com>
2024-01-18 15:15:33 -0800

    consolidate

--- js/samples/01.messaging.a.echoBot/src/app.ts
+++ js/samples/01.messaging.a.echoBot/src/app.ts
@@ -1,15 +1,31 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { Application, TurnState } from '@microsoft/teams-ai';
-import { ActivityTypes, MemoryStorage, TurnContext } from 'botbuilder';
+import debug from 'debug';
+import { Application, TurnState, TeamsAdapter } from '@microsoft/teams-ai';
+import {
+    ActivityTypes,
+    MemoryStorage,
+    TurnContext,
+    ConfigurationServiceClientCredentialFactory
+} from 'botbuilder';
+
+const error = debug('echo:app:error');
 
 interface ConversationState {
     count: number;
 }
 
 export const app = new Application<TurnState<ConversationState>>({
-    storage: new MemoryStorage()
+    storage: new MemoryStorage(),
+    adapter: new TeamsAdapter(
+        {},
+        new ConfigurationServiceClientCredentialFactory({
+            MicrosoftAppId: process.env.BOT_ID,
+            MicrosoftAppPassword: process.env.BOT_PASSWORD,
+            MicrosoftAppType: 'MultiTenant'
+        })
+    )
 });
 
 // Listen for user to say '/reset' and then delete conversation state
@@ -23,4 +39,24 @@ app.activity(ActivityTypes.Message, async (context: TurnContext, state: TurnStat
     let count = state.conversation.count ?? 0;
     state.conversation.count = ++count;
     await context.sendActivity(`[${count}] you said: ${context.activity.text}`);
+});
+
+app.error(async (context: TurnContext, err: any) => {
+    // This check writes out errors to console log .vs. app insights.
+    // NOTE: In production environment, you should consider logging this to Azure
+    //       application insights.
+    error(`[onTurnError] unhandled error: ${err}`);
+    error(err);
+
+    // Send a trace activity, which will be displayed in Bot Framework Emulator
+    await context.sendTraceActivity(
+        'OnTurnError Trace',
+        `${err}`,
+        'https://www.botframework.com/schemas/error',
+        'TurnError'
+    );
+
+    // Send a message to the user
+    await context.sendActivity('The bot encountered an error or bug.');
+    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
 });
--- js/samples/01.messaging.a.echoBot/src/index.ts
+++ js/samples/01.messaging.a.echoBot/src/index.ts
@@ -3,9 +3,9 @@
 
 import express from 'express';
 import debug from 'debug';
+import { TeamsAdapter } from '@microsoft/teams-ai';
 
 import { app } from './app';
-import { adapter } from './adapter';
 
 const log = debug('echo:server');
 const port = process.env.port || process.env.PORT || 3978;
@@ -13,7 +13,7 @@ const server = express().use(express.json());
 
 server.post('/api/messages', async (req, res) => {
     // Route received a request to adapter for processing
-    await adapter.process(req, res as any, async (context) => {
+    await (app.adapter as TeamsAdapter).process(req, res as any, async (context) => {
         // Dispatch to application for routing
         await app.run(context);
     });
